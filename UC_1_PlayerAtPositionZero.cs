73826b2cbf9de5dbb4f8f90e27dd3225a7843262
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 15:01:25 +0530

    [Ravina] Add : Showing the Player At position Zero

---
+++ UC_1_PlayerAtPositionZero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC_1_PlayerAtPositionZero
+{
+    class UC_1_PlayerAtPositionZero
+    {
+        static void Main(string[] args)
+        {
+            int player = 1;
+            int StartingPosition = 0;
+            //computation
+            Console.WriteLine("No. of Player " + player);
+            Console.WriteLine("Position at start:- " + StartingPosition);
+            Console.ReadLine();
+        }
+    }
+}

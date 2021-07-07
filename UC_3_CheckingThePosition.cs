f3640fe2b074db8489a262219a3f5fd0e19de7d7
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 15:12:35 +0530

    [Ravina] Add : Checking The Position

---
+++ UC_3_CheckingThePosition.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace UC_3_CheckingThePosition
+{
+     class UC_3_CheckingThePosition
+    {
+
+
+        public const int NoPlay = 0;
+        public const int GotLadder = 1;
+        public const int SnakeBite = 2;
+        static void Main(string[] args)
+        {
+            //variable
+
+
+
+            //computation
+
+            Random random = new Random();
+            int DiceRoll = random.Next(1, 7);
+
+
+            Console.WriteLine("You are getting the Number:-" + DiceRoll);
+
+
+
+            Random random1 = new Random();
+            int Check = random1.Next(0, 3);
+
+            Console.WriteLine("Checking The status:" + Check);
+            if (Check == NoPlay)
+            {
+                Console.WriteLine("The player stays in the same Position");
+
+            }
+            else if (Check == GotLadder)
+            {
+                Console.WriteLine("Well done You are moving Forward By: " + DiceRoll);
+
+
+            }
+            else if (Check == SnakeBite)
+            {
+                Console.WriteLine(" Oops You are moving Backward by:" + DiceRoll);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}

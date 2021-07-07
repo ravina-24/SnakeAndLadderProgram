7973100adf795601f2c4055f1b3eb9dd8c208b2e
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 15:17:58 +0530

    [ravina] Add : Repeating the Position Till Player Wins

---
+++ UC_4_RepeatTillWin.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace UC_4_RepeatTillWin
+{
+   public  class UC_4_RepeatTillWin
+
+    {
+        public const int NoPlay = 0;
+        public const int GotLadder = 1;
+        public const int SnakeBite = 2;
+        public const int WinningP = 100;
+        static void Main(string[] args)
+        {
+            //variable
+
+
+            int StartingP = 1;
+            int CurrentP = 0;
+
+
+
+            //computation
+            while (StartingP <= WinningP)
+            {
+
+
+                //Generating the Random dice number 
+                Random random = new Random();
+                int DiceRoll = random.Next(1, 7);
+                Console.WriteLine("You are getting the Number:" + DiceRoll);
+
+
+
+                //checking the options                    
+                Random random1 = new Random();
+                int Check = random1.Next(0, 3);
+
+                CurrentP = StartingP + DiceRoll;
+
+                if (CurrentP == 100)
+                {
+
+                    StartingP = StartingP - DiceRoll;
+                    Console.WriteLine("....................You won the game...................... ");
+
+                    break;
+                }
+                else if (CurrentP > 100)
+                {
+
+                    Console.WriteLine("you do not have any option and you will have to stay in that position: " + DiceRoll);
+
+                    continue;
+
+
+
+                }
+
+
+                if (Check == NoPlay)
+                {
+
+                    Console.WriteLine("You do not have any option ");
+                    Console.WriteLine("You are staying in that position");
+
+
+                }
+                else if (Check == GotLadder)
+                {
+                    StartingP = StartingP + DiceRoll;
+                    Console.WriteLine("You are moving forward by" + DiceRoll);
+                }
+                else if (Check == SnakeBite)
+                {
+                    StartingP = StartingP - DiceRoll;
+                    Console.WriteLine("You are moving Backward by" + DiceRoll);
+
+                }
+
+                if (StartingP <= 0)
+                {
+
+
+                    StartingP = StartingP - StartingP;
+                    Console.WriteLine("...............You lost the Game........................:");
+                    Console.WriteLine(".............Restart the Game............");
+
+
+                }
+
+
+
+            }
+        }
+    }
+}
+
+        

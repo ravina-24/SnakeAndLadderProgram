d4d3a8a1b2c85200ef44bb767ca81a87b1af93f9
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 15:08:08 +0530

    [Ravina] Add : Rolling the dice

---
+++ UC_2_RollTheDice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC_2_RolltheDice
+{
+    class UC_2_RollTheDice
+    {
+        static void Main(string[] args)
+        {
+            //variable
+            int player = 1;
+            int StartingP = 0;
+
+            //computation
+
+            Random random = new Random();
+            int DiceRoll = random.Next(1, 7);
+
+            Console.WriteLine("No. of Player " + player);
+
+            Console.WriteLine("You got number :" + DiceRoll);
+            Console.ReadLine();
+        }
+    }
+}

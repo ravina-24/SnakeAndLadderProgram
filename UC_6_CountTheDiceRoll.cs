c1f50a5f921b60ca397b320a77d4da3bc2494e76
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 15:32:11 +0530

    [Ravina] Add: Count the Dice Roll and Show the position

---
+++ UC_6_CountTheDiceRoll.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace UC_6_CountTheDiceRoll
+{
+    public  class UC_6_CountTheDiceRoll
+    {
+
+        public const int NoPlay = 0;
+        public const int GotLadder = 1;
+        public const int SnakeBite = 2;
+        public const int WinningP = 100;
+        static void Main(string[] args)
+        {
+            //variable
+
+
+            int StartingP = 1;
+            int CurrentP = 0;
+
+            int count = 0;
+
+            //computation
+            while (StartingP <= WinningP)
+            {
+
+
+                //Generating the Random dice number 
+                Random random = new Random();
+                int DiceRoll = random.Next(1, 7);
+                Console.WriteLine("You are getting the Number:" + DiceRoll);
+
+
+
+                //checking the options                    
+                Random random1 = new Random();
+                int Check = random1.Next(0, 3);
+
+                CurrentP = StartingP + DiceRoll;
+                count++;
+                if (CurrentP == 100)
+                {
+
+                    StartingP = StartingP - DiceRoll;
+                    Console.WriteLine("....................You won the game...................... ");
+                    Console.WriteLine("The Dice is thown by " + count);
+                    break;
+                }
+                else if (CurrentP > 100)
+                {
+
+                    Console.WriteLine("you do not have any option and you will have to stay in that position: " + DiceRoll);
+                    Console.WriteLine("You are at position :" + StartingP);
+                    continue;
+
+
+
+                }
+
+
+                if (Check == NoPlay)
+                {
+
+                    Console.WriteLine("You do not have any option ");
+                    Console.WriteLine("You are staying in that position");
+
+
+                }
+                else if (Check == GotLadder)
+                {
+                    StartingP = StartingP + DiceRoll;
+                    Console.WriteLine("You are moving forward by" + DiceRoll);
+                }
+                else if (Check == SnakeBite)
+                {
+                    StartingP = StartingP - DiceRoll;
+                    Console.WriteLine("You are moving Backward by" + DiceRoll);
+
+                }
+
+                if (StartingP <= 0)
+                {
+
+
+                    StartingP = StartingP - StartingP;
+                    Console.WriteLine("...............You lost the Game........................:");
+                    Console.WriteLine(".............Restart the Game............");
+
+
+                }
+
+                Console.WriteLine("Your position is " + StartingP);
+                Console.WriteLine("Dice is thrown by :" + count + "times");
+
+            }
+        }
+    }
+}

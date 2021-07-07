b134a0aa96ce436ad2712b75b1f83c3379d8d613
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-07 14:58:41 +0530

    [Ravina]Add : Welcome to Snake And Ladder Application

---
+++ SnakeAndLadderProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeAndLadderProgram
+{
+    class SnakeAndLadderProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome  to Snake and Ladder Application");
+            Console.ReadLine();
+        }
+
+        
+    }
+}
